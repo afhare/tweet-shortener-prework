80535dbf53153647edf5296dd511c988c6c05c10
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-21 14:19:05 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,8 @@ end
 def shortened_tweet_truncator(tweet)
   if tweet.chars.length > 140
     short_tweet = word_substituter(tweet)[0..136] + "..."
-    
+  else
+    word_substituter(tweet)
   end
   short_tweet
 end
