ae98b5f0f2c2e41aff3bcc80c1dc334e3ed3d210
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-21 14:20:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,8 +16,10 @@ end
 
 def word_substituter (tweet)
   tweet.split(" ").map do |word|
-    if dictionary.keys.include?(word.downcase)
+   if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
+    else
+      word
     end
   end.join(" ")
 end
